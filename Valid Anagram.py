ed507885548fa2cf713a6e6828122e317dcd807c
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-09-05 22:18:44 +0530

    feat: add implementation of isAnagram function to check if two strings are anagrams

---
+++ Valid Anagram.py
@@ -0,0 +1,48 @@
+s = "anagram"
+t = "nagara"
+
+class Solution(object):
+    def isAnagram( s, t):
+
+        frequency_s = {}
+        frequency_t = {}
+
+# count characters in s
+
+# count characters in t 
+
+        if len(s) != len(t):
+            return False
+
+        for character in s:
+
+          frequency_s[character] = frequency_s.get(character, 0) + 1
+
+        for char in  t :
+            
+          frequency_t[char] = frequency_t.get(char, 0) + 1
+
+
+        if frequency_t != frequency_s:
+              return False
+
+        return True
+
+
+f = Solution()
+
+print(Solution.isAnagram(s,t))
+
+        
+
+        
+
+            
+
+
+            
+        # increase frequency of character
+                
+                
+            
+                   
