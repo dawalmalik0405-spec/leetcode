a6afc55999c1b8c194ab11d7c51c221aece2d5ab
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-09-06 22:04:16 +0530

    feat: refactor valid_parenthesis function to improve readability and remove unused code

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,24 +1,62 @@
-s ="(]"
+# s ="(]"
 
-v = []
-pairs = {')': '(', ']': '[', '}': '{'}
+# v = []
+# pairs = {')': '(', ']': '[', '}': '{'}
 
-for i in s:
+# for i in s:
+#     if i in "([{":
+#       v.append(i)
+
+#       print(v)
+#     elif i in ")]}":
+#       if not v:
+#         print("Invalid")
+#         break
+#       elif v[-1] == pairs[i] :
+#          v.pop()
+#       else:
+#         print("invalid")
+#         break
+# else:
+#   if not v:
+#       print("Valid")
+#   else:
+#       print("Invalid")
+
+
+s = "({[]})"
+
+
+mapping = {
+    ")": "(",
+    "]": "[",
+    "}": "{"
+}
+def valid_parenthesis(s):
+  stack = []
+
+
+  for i in s:
     if i in "([{":
-      v.append(i)
+      stack.append(i)
 
-      print(v)
-    elif i in ")]}":
-      if not v:
-        print("Invalid")
-        break
-      elif v[-1] == pairs[i] :
-         v.pop()
+    else:
+      if not stack:
+        return False
+
+      if stack[-1] != mapping[i]:
+        return False
+ 
       else:
-        print("invalid")
-        break
-else:
-  if not v:
-      print("Valid")
-  else:
-      print("Invalid")
+        stack.pop()
+
+  return not stack
+
+
+print(valid_parenthesis(s))
+      
+
+
+
+
+
