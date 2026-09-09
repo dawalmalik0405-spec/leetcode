e0d10c481d508cf8c562b376c87513e3e771fdf7
dawalmalik0405-spec <dawalmalik0405@gmail.com>
2026-09-09 14:53:29 +0530

    feat: implement tree_order_traversal function for level order traversal of a binary tree

---
+++ Binary Tree Level Order Traversal.py
@@ -0,0 +1,33 @@
+def tree_order_traversal(root):
+
+    if root is None:
+        return []
+
+    queue = [root]
+    result = []
+
+    while queue:
+        current_level = []
+
+        for i in range(len(queue)):
+
+            
+            node = queue.pop(0)
+            current_level.append(node.val)
+            
+
+            if node.left:
+                queue.append(node.left)
+                
+
+            if node.right:
+                queue.append(node.right)
+                
+
+            
+
+        result.append(current_level)
+
+    return result
+
+  
